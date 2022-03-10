ded8476d11aac0cbd7f794e79c9bcf4f099e0b51
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-03-10 06:47:27 -0500

    Added a resolver for feed root field and to resolve each field on the Link type

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const {ApolloServer} = require('apollo-server');
 
 //1
 const typeDefs = `
-  //Query root type has currently two root fields - info and feed
+  
   type Query {
     info: String!
     feed: [Link!]!
   }
 
-  //type definition for the Link object type
+  
   type Link {
     id: ID!
     description: String!
@@ -18,10 +18,27 @@ const typeDefs = `
   }
 `
 
+
+//dummy list of links
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+
+}]
+
+
 //2 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
